f0f745ce3d8a6ea91307ed20f5739dc0b9e4ffa5
nbradley23 <n.bradley23@yahoo.com>
2023-03-10 19:28:38 -0600

    work folder commit

---
+++ cdn-specific-results.py
@@ -0,0 +1,11 @@
+from cdn_dns_lookup import site_and_cdn
+
+print(site_and_cdn)
+
+# cloudflare_dict = {}
+
+# for site in site_and_cdn:
+#     if 'cloudflare' in site_and_cdn.get(site):
+#         cloudflare_dict[site] = site_and_cdn.get(site)
+
+# print(cloudflare_dict)
